9a9c1007b20c0d6fef6aeb76cfbe4c4fc37ce568
BishoyHanyRaafat <bishoyhany006@gmail.com>
2024-04-06 14:36:25 +0200

    more readable conversation

--- src/pieces/commands/copilot/conversations.py
+++ src/pieces/commands/copilot/conversations.py
@@ -4,8 +4,28 @@ from pieces.api.config import api_client
 
 
 
+def conversation_handler(**kwargs):
+    get_conversations()
+
+
 def get_conversations():
     api_instance = pos_client.ConversationsApi(api_client)
     api_response = api_instance.conversations_snapshot()
-    pydoc.pager("\n".join([f"{idx}. {conversation.name}" for idx,conversation in enumerate(api_response.iterable,1)]))
+
+    # Sort the dictionary by the "updated" timestamp
+    sorted_conversations = sorted(api_response.iterable, key=lambda x: x.updated.value, reverse=True)
+
+    
+    readable = sorted_conversations[0].updated.readable
+    conversations_str = f"            {readable}\n"
+    for idx,conversation in enumerate(sorted_conversations,1):
+        if conversation.updated.readable != readable:
+            readable = conversation.updated.readable
+            conversations_str += f"__________________________________________________\n\n            {readable}\n"
+        if not conversation.name:
+            conversation.name = "New conversation"
+        conversations_str += f"{idx}. {conversation.name}\n"
+        
+    pydoc.pager(conversations_str)
+
 
