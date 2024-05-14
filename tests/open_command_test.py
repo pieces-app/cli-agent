b81cd4b50f6e8b5697014616dce57ab05db97de3
Bishoy-at-pieces <bishoy@pieces.app>
2024-05-14 20:10:23 +0300

    refactor: update open command test

--- tests/open_command_test.py
+++ tests/open_command_test.py
@@ -1,34 +1,31 @@
 import unittest
 from unittest.mock import patch, MagicMock
-from pieces.commands import startup,open_asset,list_assets
+from pieces.assets import AssetsCommands,AssetsCommandsApi
+from pieces.settings import Settings
 import sys
 from io import StringIO
 import random,os
 import json
 from pieces.utils import sanitize_filename
 
-class TestAskCommand(unittest.TestCase):
-    @patch('pieces.commands.open_asset')
-    def test_open_command(self, mock_open_question):
-        # get all the list assets 
-        startup()
+class TestOpenCommand(unittest.TestCase):
+    def test_open_command(self):
+        Settings.startup()
 
         stdout = sys.stdout
         sys.stdout = StringIO()
 
-        list_assets()
 
-        # Get the output and restore stdout
-        result_list = sys.stdout.getvalue()
-
-        assets_length = len(result_list.strip().split('\n'))
+        assets_length = len(AssetsCommandsApi.get_assets_snapshot())
 
 
         sys.stdout = StringIO()
+        idx = random.randint(1,assets_length)
         # Act
-        open_asset(ITEM_INDEX =random.randint(1,assets_length))
+        AssetsCommands.open_asset(ITEM_INDEX = idx)
 
         result_open = sys.stdout.getvalue()
+        
         sys.stdout = stdout  # Reset sys.stdout to its original state
 
         result_list = result_open.strip().split('\n')
@@ -41,12 +38,15 @@ class TestAskCommand(unittest.TestCase):
         code_snippet_path = result_list[-1].removeprefix('Code: ')
 
 
-        with open("src/pieces/commands/extensions.json") as f:
+        with open(Settings.extensions_dir) as f:
             language_extension_mapping = json.load(f)
         self.assertTrue(os.path.exists(code_snippet_path))  # assert that the code snippet file exists
         self.assertEqual(os.path.splitext(code_snippet_path)[-1], language_extension_mapping[language])  # assert that the file extension matches the language
         self.assertEqual(os.path.splitext(os.path.basename(code_snippet_path))[0], sanitize_filename(name))
 
+
+if __name__ == '__main__':
+    unittest.main()
         
 
         
