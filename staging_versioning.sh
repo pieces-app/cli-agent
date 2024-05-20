73fc394c601725eab8bfd17ad6ad732d28dea566
Bishoy-at-pieces <bishoy@pieces.app>
2024-05-20 19:48:57 +0300

    fix the staging version logic

--- staging_versioning.sh
+++ staging_versioning.sh
@@ -5,13 +5,13 @@
 prod_json="$(curl https://storage.googleapis.com/app-releases-production/pieces_cli/production/latest-single.json -H "Accept: application/json")"
 
 # Get First PROD JSON Object in an array, and get the "tag" value
-prod_tag=$(echo "$prod_json" | jq '.tag')
+prod_tag=$(echo "$prod_json" | jq -r '.tag')
 
 # Get STAGING JSON With GET Request
 staging_json="$(curl https://storage.googleapis.com/app-releases-staging/pieces_cli/staging/latest-single.json -H "Accept: application/json")"
 
 # Get First JSON Object in an array, and get the "tag" value
-staging_tag=$(echo "$staging_json" | jq '.tag')
+staging_tag=$(echo "$staging_json" | jq -r '.tag')
 
 # Remove "-staging" from tag to manipulate numbers
 staging_tag=${staging_tag//"-staging"/}
@@ -20,21 +20,18 @@ staging_tag=${staging_tag//"-staging"/}
 #------------------------------- Version Extraction ---------------------
 
 # Extract version numbers from production and staging tags
-prod_tag_pure_num=$(echo "$prod_tag" | sed 's/"//g')
-prod_major=$(echo "$prod_tag_pure_num" | cut -d. -f1)
-prod_minor=$(echo "$prod_tag_pure_num" | cut -d. -f2)
-prod_patch=$(echo "$prod_tag_pure_num" | cut -d. -f3)
+prod_major=$(echo "$prod_tag" | cut -d. -f1)
+prod_minor=$(echo "$prod_tag" | cut -d. -f2)
+prod_patch=$(echo "$prod_tag" | cut -d. -f3)
 
-staging_tag_pure_num=$(echo "$staging_tag" | sed 's/"//g')
-staging_major=$(echo "$staging_tag_pure_num" | cut -d. -f1)
-staging_minor=$(echo "$staging_tag_pure_num" | cut -d. -f2)
-staging_patch=$(echo "$staging_tag_pure_num" | cut -d. -f3)
-staging_dev=$(echo "$staging_tag_pure_num" | grep -oP '(?<=\.dev)\d+' || echo 0)
+staging_major=$(echo "$staging_tag" | cut -d. -f1)
+staging_minor=$(echo "$staging_tag" | cut -d. -f2)
+staging_patch=$(echo "$staging_tag" | cut -d. -f3)
+staging_dev=$(echo "$staging_tag" | grep -oP '(?<=\.dev)\d+' || echo 0)
 
 #---------------------------------------------------------------------------------
 
 #------------------------------- Staging Versioning Logic ------------------------
-
 
 # Staging Versioning Logic
 # If prod major or minor is higher than staging, update staging major and minor and reset dev to 1
@@ -45,8 +42,12 @@ elif [ "$prod_major" -eq "$staging_major" ] && [ "$prod_minor" -eq "$staging_min
     final_version="$prod_major.$prod_minor.$prod_patch.dev1"
 # If prod hasn't changed, increment the dev number
 else
-    increment_dev=$(expr $staging_dev + 1)
-    final_version="$staging_major.$staging_minor.$staging_patch.dev$increment_dev"
+    if [ "$staging_dev" -eq 0 ]; then
+        final_version="$staging_major.$staging_minor.$staging_patch.dev1"
+    else
+        increment_dev=$(expr $staging_dev + 1)
+        final_version="$staging_major.$staging_minor.$staging_patch.dev$increment_dev"
+    fi
 fi
 
 echo $final_version
