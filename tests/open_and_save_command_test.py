72f3e4249ec7e2456883c1817505dc7b972e36af
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-07-30 23:19:01 +0530

    Refactoring and asset cache
    - Minor refactoring changes here and there
    - Updating the Asset Cache in mocks

--- tests/open_and_save_command_test.py
+++ tests/open_and_save_command_test.py
@@ -20,7 +20,7 @@ class TestOpenSaveCommand(unittest.TestCase):
         AssetsCommandsApi.assets_api = self.mock_assets_api
         AssetsCommandsApi.asset_api = self.mock_asset_api
         AssetsCommandsApi.format_api = self.mock_format_api
-    
+
     def test_open_command(self, ITEM_INDEX=None):
         Settings.startup()
         stdout = sys.stdout
@@ -29,11 +29,11 @@ class TestOpenSaveCommand(unittest.TestCase):
         sys.stdout = StringIO()
         if not ITEM_INDEX:
             ITEM_INDEX = random.randint(1, assets_length)
-
+        
         # Act
         AssetsCommands.open_asset(ITEM_INDEX=ITEM_INDEX)
         result_open = sys.stdout.getvalue()
-
+        
         sys.stdout = stdout  # Reset sys.stdout to its original state
         result_list = result_open.strip().split('\n')
         name = result_list[-6].removeprefix('Name: ')
@@ -42,17 +42,18 @@ class TestOpenSaveCommand(unittest.TestCase):
         type = result_list[-3].removeprefix('Type: ')
         language = result_list[-2].removeprefix('Language: ')
         code_snippet = result_list[-1].removeprefix('Code content: ')
+        
 
         # Test 1: Verify that the code snippet is not empty
         self.assertTrue(code_snippet.strip(), "The code snippet should not be empty")
-
         # Test 2: Verify that the language is valid
         is_valid_language, identified_language = self.verify_language_with_pygments(code_snippet)
         self.assertTrue(is_valid_language, f"The code snippet should be in a valid programming language. Identified as: {identified_language}")
-
+        
         print("test_open_command passed successfully")
 
-     def verify_language_with_pygments(self, code_content):
+
+    def verify_language_with_pygments(self, code_content):
         try:
             # Try to guess the lexer based on the code content
             lexer = guess_lexer(code_content)
@@ -60,7 +61,7 @@ class TestOpenSaveCommand(unittest.TestCase):
         except ClassNotFound:
             # If we can't guess the lexer, assume the language is invalid
             return False, "Unknown"
-        
+
     @patch('builtins.input', side_effect=['y','y'])
     @patch('pyperclip.paste', return_value='print("Hello, World!")')
     def test_save_command(self, mock_paste, mock_buildins):
@@ -69,21 +70,30 @@ class TestOpenSaveCommand(unittest.TestCase):
         mock_new_asset.id = 'new_asset_id'
         mock_new_asset.name = "Print Hello World in Python"
         self.mock_asset_api.asset_create.return_value = mock_new_asset
-
+        
         with patch('builtins.print'):  # Suppress print statements
             AssetsCommands.create_asset()
+        
+        # Check if the asset was created
+        self.assertTrue(mock_new_asset.name, "Asset name is empty")
+        print("Asset creation successful")
+
+        # Update the asset cache
+        AssetsCommandsApi._assets_snapshot = {mock_new_asset.id: mock_new_asset}
+        AssetsCommands.current_asset = mock_new_asset.id
+
+        
 
         # Check if the code was saved
         updated_asset = AssetsCommandsApi.update_asset_snapshot(AssetsCommands.current_asset)
         self.assertTrue(updated_asset.formats.iterable[0].fragment.string.raw, "Updated asset content is empty")
-        print("Asset update successful")
+        print("Asset updated successful")
 
         # Simulate deleting the asset
         with patch('builtins.print'):  # Suppress print statements
             AssetsCommands.delete_asset()
-
+        
         print("test_save_command passed successfully")
 
 if __name__ == '__main__':
     unittest.main()
-
