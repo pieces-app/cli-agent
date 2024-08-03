0f395efd51b095ab13b8695557ca7132928b91bd
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-08-04 01:02:12 +0530

    Updated the Asset_identifier class
    updated class to work accordingly , inheriting necessary functions from the base class

--- src/pieces/assets/assets_identifiers_ws.py
+++ src/pieces/assets/assets_identifiers_ws.py
@@ -67,34 +67,13 @@ class BaseWebsocket(ABC):
         for instance in cls.instances:
             instance.close_websocket_connection()
 
-class AssetsIdentifiersWS:
-    def __new__(cls,*args,**kwargs):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(AssetsIdentifiersWS, cls).__new__(cls)
-        return cls.instance
-    
+class AssetsIdentifiersWS(BaseWebsocket):
     def __init__(self, on_message_callback):
-        self.ws = None
-        self.on_message_callback = on_message_callback
+        super().__init__(on_message_callback)
+        self.start()  # Using the start method from BaseWebsocket
 
-        # Create a new event loop
-        self.loop = asyncio.new_event_loop()
-
-        # Run the event loop in a new thread
-        self.thread = threading.Thread(target=self.open_websocket)
-        self.thread.start()
-    
-
-    def open_websocket(self):
-        """Opens a websocket connection"""
-        if self.ws: # connect only once
-            return
-        self.ws = websocket.WebSocketApp(Settings.ASSETS_IDENTIFIERS_WS_URL,
-                                         on_message=self.on_message,
-                                         on_error=self.on_error,
-                                         on_close=self.on_close)
-        self.ws.on_open = self.on_open
-        self.ws.run_forever()
+    def get_url(self):
+        return Settings.ASSETS_IDENTIFIERS_WS_URL
 
     def on_message(self, ws, message):
         """Handle incoming websocket messages."""
@@ -102,18 +81,27 @@ class AssetsIdentifiersWS:
 
     def on_error(self, ws, error):
         """Handle websocket errors."""
-        self.ws = None
+        super().on_error(ws, error)
         print(error)
 
     def on_close(self, ws, close_status_code, close_msg):
         """Handle websocket closure."""
-        self.ws = None
+        super().on_close(ws, close_status_code, close_msg)
 
     def on_open(self, ws):
         """Handle websocket opening."""
-        pass
+        super().on_open(ws)
+
+    def open_websocket(self):
+        """Opens a websocket connection"""
+        super().open_websocket()
 
     def close_websocket_connection(self):
         """Close the websocket connection."""
-        if self.ws:
-            self.ws.close()
+        super().close_websocket_connection()
+
+    @classmethod
+    def close_all(cls):
+        """Close all websocket connections."""
+        super().close_all()
+
