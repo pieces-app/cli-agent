13c96ea788c4fc7b91c8d70549935f3d2a7ee81c
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-08-14 00:40:40 +0530

    Execute command Class initialisation
    started with basic functionality of execute command

--- src/pieces/commands/run_shell_command.py
+++ src/pieces/commands/run_shell_command.py
@@ -7,3 +7,15 @@ import shlex
 from list_command import ListCommand, PiecesSelectMenu
 # Import necessary functions from assets_command.py
 from pieces.assets.assets_command import AssetsCommands
+
+class ExecuteCommand:
+    @classmethod
+    def execute_command(cls, **kwargs):
+        type = kwargs.get("type", "assets")
+        max_assets = kwargs.get("max_assets", 10)
+        if max_assets < 1:
+            print("Max assets must be greater than 0")
+            max_assets = 10
+        
+        if type == "assets":
+            cls.execute_assets(max_assets)
