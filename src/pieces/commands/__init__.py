5955964033d06b9469ccd24c6c1a092ec90041fa
Bishoy-at-pieces <bishoy@pieces.app>
2024-09-24 20:08:08 +0300

    Update __init__.py

--- src/pieces/commands/__init__.py
+++ src/pieces/commands/__init__.py
@@ -3,7 +3,7 @@ from .change_model import change_model
 from .search_command import search
 from .list_command import ListCommand
 from .version_command import version
-from .signout_command import sign_out
+from .auth_commands import sign_in, sign_out
 from .config_command import ConfigCommands
 from .execute_command import ExecuteCommand
 
@@ -12,6 +12,7 @@ __all__ = ['loop',
            'search',
            'change_model',
            'sign_out',
+           'sign_in',
            'ListCommand',
            'ConfigCommands',
            "ExecuteCommand"]
