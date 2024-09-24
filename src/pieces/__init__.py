e760005d2fb251cb3ba0f4ba3286d1f28e5cdcf0
Bishoy-at-pieces <bishoy@pieces.app>
2024-09-24 19:07:15 +0300

    Update __init__.py

--- src/pieces/__init__.py
+++ src/pieces/__init__.py
@@ -1,8 +1,2 @@
-import importlib.metadata
-
-try:
-    __version__ = importlib.metadata.version("pieces-cli")
-except importlib.metadata.PackageNotFoundError:
-    print("Could not find the 'pieces-cli' package in the Python environment. Is it installed?")
-    __version__ = "unknown"
+__version__ = "1.3.1"
 
