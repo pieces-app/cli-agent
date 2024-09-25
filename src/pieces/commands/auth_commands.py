a8e70ce5fe40db53a1c6a8e39632c2ddc32da2dc
Bishoy-at-pieces <bishoy@pieces.app>
2024-09-26 02:04:29 +0300

    fix missing kwargs

--- src/pieces/commands/auth_commands.py
+++ src/pieces/commands/auth_commands.py
@@ -1,12 +1,12 @@
 from pieces.settings import Settings
 
-def sign_out():
+def sign_out(**kwargs):
     try:
         Settings.pieces_client.user.logout()
     except:
         pass
 
-def sign_in():
+def sign_in(**kwargs):
     try:
         Settings.pieces_client.user.login()
     except:
