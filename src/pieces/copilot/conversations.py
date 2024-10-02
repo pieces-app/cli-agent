4af4bff44751dfa9f66fba626a4491ffb330110b
Bishoy Hany <167128903+Bishoy-at-pieces@users.noreply.github.com>
2024-10-02 20:20:04 +0300

    Update conversations.py

--- src/pieces/copilot/conversations.py
+++ src/pieces/copilot/conversations.py
@@ -95,7 +95,7 @@ def get_conversations(max_conversations,**kwargs):
             else:
                 # If the string is 20 characters or less, use it as is
                 truncated_summary = summary_str
-            output.append(f'  {"\n  ".join(truncated_summary.split("\n"))}\n', style="dim")
+            output.append(f'  {"\n  ".join(truncated_summary.split("\n"))}'+"\n", style="dim")
 
     console.print(output)
 
