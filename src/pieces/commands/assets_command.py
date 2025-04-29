d986986efc0695dc0ccc953e7f6f3ff722ef1439
bishoy-at-pieces <bishoy@pieces.app>
2025-04-29 13:40:24 +0300

    fix editor loop

--- src/pieces/commands/assets_command.py
+++ src/pieces/commands/assets_command.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from typing import Optional
+from typing import Optional, Tuple
 import pyperclip
 import subprocess
 
@@ -75,47 +75,57 @@ class AssetsCommands:
 
         # Check if -e flag is used or open_in_editor is True
         if open_in_editor:
-            config = ConfigCommands.load_config()
-            editor = config.get("editor")
-            if editor:
-                file_extension = get_file_extension(
-                    cls.current_asset.classification)
+            check, editor = cls.check_editor()
+            if not check:
+                return
+            file_extension = get_file_extension(cls.current_asset.classification)
 
-                # Ensure the directory exists, create it if not
-                if not os.path.exists(Settings.open_snippet_dir):
-                    os.makedirs(Settings.open_snippet_dir)
+            # Ensure the directory exists, create it if not
+            if not os.path.exists(Settings.open_snippet_dir):
+                os.makedirs(Settings.open_snippet_dir)
 
-                file_path = os.path.join(
-                    Settings.open_snippet_dir, f"{cls.current_asset.id}{file_extension}"
-                )
+            file_path = os.path.join(
+                Settings.open_snippet_dir, f"{cls.current_asset.id}{file_extension}"
+            )
 
-                # Save the code to a file in the default directory
-                if isinstance(code_content, str):  # Code string raw
-                    with open(file_path, "w") as file:
-                        file.write(code_content)
-                else:  # Image bytes data
-                    with open(file_path, "wb") as file:
-                        file.write(bytes(code_content))
+            # Save the code to a file in the default directory
+            if isinstance(code_content, str):  # Code string raw
+                with open(file_path, "w") as file:
+                    file.write(code_content)
+            else:  # Image bytes data
+                with open(file_path, "wb") as file:
+                    file.write(bytes(code_content))
 
-                # Open the file with the configured editor
-                try:
-                    subprocess.run([editor, file_path])
-                except Exception as e:
-                    Settings.show_error("Error in opening", e)
+            # Open the file with the configured editor
+            try:
+                subprocess.run([editor, file_path])
+            except Exception as e:
+                Settings.show_error("Error in opening", e)
 
-            else:
-                print_asset_details(cls.current_asset)
-                Console().print(
-                    Markdown(
-                        "No editor configured. Use `pieces config --editor <editor_command>` to set an editor."
-                    )
-                )
         else:
             # Determine the lexer
             print("\nCode content:")
             cls.print_code(
                 cls.current_asset.raw_content, cls.current_asset.classification
             )
+
+    @classmethod
+    def check_editor(cls) -> Tuple[bool, str]:
+        if not cls.current_asset:
+            return False, ""
+        config = ConfigCommands.load_config()
+        editor = config.get("editor")
+
+        if editor:
+            return True, editor
+        else:
+            print_asset_details(cls.current_asset)
+            Console().print(
+                Markdown(
+                    "No editor configured. Use `pieces config --editor <editor_command>` to set an editor."
+                )
+            )
+            return False, ""
 
     @staticmethod
     def print_code(code_content, classification=None):
@@ -132,9 +142,11 @@ class AssetsCommands:
     @classmethod
     @check_asset_selected
     def save_asset(cls, asset: BasicAsset, **kwargs):
-        if not asset:
-            return
         console = Console()
+
+        if not cls.check_editor()[0]:
+            return
+
         file_path = os.path.join(
             Settings.open_snippet_dir,
             f"{(asset.id)}{get_file_extension(asset.classification)}",
@@ -230,8 +242,7 @@ class AssetsCommands:
                 return
 
         if not text:
-            console.print(
-                "No content found in the clipboard to create a material.")
+            console.print("No content found in the clipboard to create a material.")
             return
 
         double_line("Content to save: ")
