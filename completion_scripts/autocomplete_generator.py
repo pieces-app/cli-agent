cc52b9a9d716a46ba2ba7b936e7f418b4f1b2063
bishoy-at-pieces <bishoy@pieces.app>
2025-06-20 15:53:21 +0300

    add some more doc comments to source the automation script

--- completion_scripts/autocomplete_generator.py
+++ completion_scripts/autocomplete_generator.py
@@ -5,6 +5,20 @@ To generate the completion scripts, run the following commands:
 python3 completion_scripts/autocomplete_generator.py --zsh >> src/pieces/completions/zsh
 python3 completion_scripts/autocomplete_generator.py --bash >> src/pieces/completions/bash
 python3 completion_scripts/autocomplete_generator.py --fish >> src/pieces/completions/fish
+
+To try it run:
+
+```bash
+source <(python3 completion_scripts/autocomplete_generator.py --bash)
+```
+
+```zsh
+source <(python3 completion_scripts/autocomplete_generator.py --zsh)
+```
+
+```fish
+python3 completion_scripts/autocomplete_generator.py --fish | source
+```
 """
 
 import sys
@@ -13,11 +27,13 @@ from base import CommandParser
 from bash import BashCompletionGenerator
 from zsh import ZshCompletionGenerator
 from fish import FishCompletionGenerator
+import signal
 
 
 cli = PiecesCLI()
 parser = CommandParser(cli.parser)
 
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
 def generate(shell_arg):
     try:
